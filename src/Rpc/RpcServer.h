8c1b94de0232d8a87561b4340cb40dbd26f01dd7
3str <hi3n.tr@gmail.com>
2018-06-12 10:53:19 +0700

    add more command

--- src/Rpc/RpcServer.h
+++ src/Rpc/RpcServer.h
@@ -84,6 +84,9 @@ private:
   RawBlockLegacy prepareRawBlockLegacy(BinaryArray&& blockBlob);
   
   bool f_on_blocks_list_json(const F_COMMAND_RPC_GET_BLOCKS_LIST::request& req, F_COMMAND_RPC_GET_BLOCKS_LIST::response& res);
+  bool f_on_block_json(const F_COMMAND_RPC_GET_BLOCK_DETAILS::request& req, F_COMMAND_RPC_GET_BLOCK_DETAILS::response& res);
+  bool f_on_transaction_json(const F_COMMAND_RPC_GET_TRANSACTION_DETAILS::request& req, F_COMMAND_RPC_GET_TRANSACTION_DETAILS::response& res);
+  bool f_on_transactions_pool_json(const F_COMMAND_RPC_GET_POOL::request& req, F_COMMAND_RPC_GET_POOL::response& res);
   
   Logging::LoggerRef logger;
   Core& m_core;
